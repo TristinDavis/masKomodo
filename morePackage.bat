1b88e86d2e205f7f78bde5b8ec969776de5f05ef
Carey Hoffman <cgchoffman@gmail.com>
2012-05-30 13:09:44 -0700

    Added morePackage.bat and comments in README about using it.  Us it to package the extension quickly in Windows.  Might add an .sh later.

---
+++ morePackage.bat
@@ -0,0 +1,18 @@
+#Thats what she said...
+@echo on
+IF EXIST morefolders RD morefolders /S
+MD morefolders\chrome
+MD morefolders\defaults
+XCOPY defaults morefolders\defaults /E
+COPY chrome.manifest morefolders
+COPY install.rdf morefolders
+CD chrome
+7z a -tzip morekomodo.zip content locale skin
+REN morekomodo.zip morekomodo.jar
+MOVE morekomodo.jar ..\morefolders\chrome
+CD ..\morefolders
+7z a -tzip moreKomodo-1.8.3.xpi chrome defaults chrome.manifest install.rdf
+CD ..
+DEL moreKomodo-1.8.3.xpi
+MOVE morefolders\moreKomodo-1.8.3.xpi .
+RD morefolders /S
